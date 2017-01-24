8c5d3c84e749baa225fec461e6368cfb295dbfe9
ron <ron@olapic.com>
2017-01-24 09:42:47 -0500

    removing old projects

--- mysite/chats/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def chats(Response):
-	return HttpResponse("This is the 'chats' view!")
--- mysite/polls/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def index(Response):
-	return HttpResponse("Hello, world. You're at the polls index.")
-
